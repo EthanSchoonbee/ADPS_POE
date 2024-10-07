4dd8ed10c8e7860d52ef84a6b8ca69f2d2a37ab5
Caleb Baard <wordwarriors52@yq0fx.onmicrosoft.com>
2024-10-07 22:51:11 +0200

    Dashboard and Payment route changed. Only authenticated users can access. So only logged in users can access the Payment and Dashboard.

--- frontend/src/Components/Payment/Payment.js
+++ frontend/src/Components/Payment/Payment.js
@@ -31,6 +31,7 @@ const Payment = () => {
     const [accountNumber, setAccountNumber] = useState("");
     const [message, setMessage] = useState(null);
     const navigate = useNavigate();
+    const [authError, setAuthError] = useState(null);
 
     // Set default values
     const [selectedCurrency, setSelectedCurrency] = useState("ZAR");
@@ -60,6 +61,26 @@ const Payment = () => {
     const [accountError, setAccountError] = useState(null);
 
     useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (!token) {
+            navigate("/");
+        }
+        else{
+            try{
+                //decoding the token
+                const decodedToken = jwtDecode(token);
+                if(decodedToken.role !== "user"){
+                    setAuthError("You don't have permission to access this page.");
+                    setTimeout(() => {
+                        navigate("/EmployeeDash");
+                    }, 3000); //Redirecting after 3 seconds
+                }
+            }
+            catch(error){
+                console.error("Error decoding token:", error);
+                navigate("/");
+            }
+        }
         setSwiftCode(bankSwiftCodes[selectedBank] || "");
     }, []);
 
@@ -104,10 +125,34 @@ const Payment = () => {
         e.preventDefault();
         setMessage(null);
 
-        // Check if the recipient does not match the input sanitization regex
-        const isNameValid = !inputSanitizationRegex.test(recipientName);
-        //if the account does not match the regex pattern
-        const isAccValid = !inputSanitizationRegex.test(accountNumber);
+        // helper function to validate fields
+        const validateInput = (input) => !inputSanitizationRegex.test(input);
+
+        //validating the recipient name
+        const isNameValid = validateInput(recipientName);
+        //validating the account number
+        const isAccValid = validateInput(accountNumber);
+
+        // Array to hold invalid fields
+        const invalidFields= [];
+
+        //checks if the recipient name and account number are valid
+        if(!isNameValid) invalidFields.push('Recipient Name');
+        if(!isAccValid) invalidFields.push('Account Number');
+
+        // if any fields are invalid, display error and clear those fields
+        if (invalidFields.length > 0) {
+            setMessage({
+                text: `Potential attack detected in: ${invalidFields.join(', ')}. Input sanitized.`,
+                type: "error",
+            });
+
+            // clear all invalid fields
+            if (!isNameValid) setRecipientName('');
+            if (!isAccValid) setAccountNumber('');
+
+            return;
+        }
 
         // Remove currency symbol and all spaces, then trim
         const numericAmount = paymentAmount
@@ -122,28 +167,6 @@ const Payment = () => {
                 type: "error",
             });
             return;
-        }
-
-        //Checks if the recipient name is not valid
-        if(!isNameValid){
-            setMessage({
-                text: "Potential attack. Input sanitized.",
-                type: "error",
-            });
-            //sets the recipient name to empty
-            setRecipientName("");
-            return;
-        }
-
-        //if the account number is not valid then will display an error message
-        if(!isAccValid) {
-            setMessage({
-                text: "Potential Attack. Input sanitized.",
-                type: "error",
-            });
-            //sets the account number to empty
-            setAccountNumber("");
-            return
         }
 
         // regex that allows for any number of digits, with optional decimal places
@@ -252,140 +275,157 @@ const Payment = () => {
     };
 
     return (
+        //The payment container
         <div className="paymentContainer">
-            <div className="paymentHeader">
-                <div className="paymentText"> Payment Portal</div>
-                <div className="paymentUnderline"></div>
-            </div>
-
-            <div className="paymentInputs">
-                {/* Transaction currency */}
-                <div className="paymentLabel">
-                    <p>Transaction Currency: </p>
-                </div>
-                <div className="paymentInput">
-                    <img src={currency_icon} alt="" />
-                    <select
-                        //dropbox for the currency
-                        className="paymentDropbox"
-                        //handling the change of the currency
-                        onChange={handleCurrencyChange}
-                        value={selectedCurrency}
-                    >
-                        <option value="ZAR">ZAR</option>
-                        <option value="USD">USD</option>
-                        <option value="GBP">GBP</option>
-                    </select>
-                </div>
-
-                {/* Payment Amount */}
-                <div className="paymentLabel">
-                    <p>Payment Amount: </p>
-                </div>
-                <div className="paymentInput">
-                    <img src={money_icon} alt="" />
-                    <input
-                        type="text"
-                        value={paymentAmount}
-                        onChange={handlePaymentAmountChange}
-                        placeholder="Payment Amount:"
-                    />
-                </div>
-
-                {/* Recipient bank */}
-                <div className="paymentLabel">
-                    <p>Recipient Bank: </p>
-                </div>
-                <div className="paymentInput">
-                    <img src={bank_icon} alt="" />
-                    <select
-                        className="paymentDropbox"
-                        //handling the change of the bank
-                        onChange={handleBankChange}
-                        value={selectedBank}
-                    >
-                        <option value="STANDARD_BANK">Standard Bank</option>
-                        <option value="ABSA">ABSA</option>
-                        <option value="FNB">FNB</option>
-                        <option value="CAPITEC">Capitec</option>
-                        <option value="NEDBANK">Nedbank</option>
-                    </select>
-                </div>
-
-                {/* SWIFT Code */}
-                <div className="paymentLabel">
-                    <p>Provider (SWIFT code): </p>
-                </div>
-                <div className="paymentInput">
-                    <img src={bank_icon} alt="" />
-                    <input
-                        type="text"
-                        value={swiftCode}
-                        placeholder="SWIFT Code"
-                        readOnly
-                    />
-                </div>
-
-                {/* Recipient name */}
-                <div className="paymentLabel">
-                    <p>Recipient Name: </p>
-                </div>
-                <div className="paymentInput">
-                    <img src={user_icon} alt="" />
-                    <input
-                        type="text"
-                        value={recipientName}
-                        onChange={handleRecipientNameChange}
-                        placeholder="  Recipient Name:"
-                    />
-                </div>
-                {nameError && <p className="error-message">{nameError}</p>}
-
-                {/* Recipient account number */}
-                <div className="paymentLabel">
-                    <p>Recipient's account no: </p>
-                </div>
-                <div className="paymentInput">
-                    <img src={card_icon} alt="" />
-                    <input
-                        type="text"
-                        value={accountNumber}
-                        onChange={handleAccountNumberChange}
-                        placeholder="Recipient's Account No"
-                    />
-                </div>
-                {accountError && <p className="error-message">{accountError}</p>}
-            </div>
-            {/* Buttons */}
-
-            <div className="paymentSubmit-container">
-                <div className="paymentSubmit" onClick={handlePayment}>
-                    Pay Now
-                </div>
-                {/* Button to cancel the payment */}
-                <div className="paymentSubmit" onClick={handleCancel}>
-                    Cancel
-                </div>
-            </div>
-
-            {/*will display the message here*/}
-            {message && (
-                <p
-                    className={
-                        message.type === "success" ? "success-message" : "error-message"
-                    }
-                >
-                    {message.text}
-                </p>
-            )}
-
-            {showModal && (
-                <div className="modal">
-                    <div className="modal-content">
-                        <h2>Payment Successful!</h2>
-                        <p>Your payment has been processed successfully.</p>
+            {/*The auth error*/}
+            {authError ? (
+                /*Getting the error container style in the Payment css and the error message*/
+                /*Will say access denied with a flash effect*/
+                <div className="error-container">
+                    <div className="errorMessage">
+                        <h2>Access Denied</h2>
+                        <p className="flash">{authError}</p>
+                        <p className="flash">Redirecting you to the Employee Dashboard...</p>
                     </div>
                 </div>
+            ) : (
+                <>
+                    <div className="paymentHeader">
+                        <div className="paymentText"> Payment Portal</div>
+                        <div className="paymentUnderline"></div>
+                    </div>
+
+                    <div className="paymentInputs">
+                        {/* Transaction currency */}
+                        <div className="paymentLabel">
+                            <p>Transaction Currency: </p>
+                        </div>
+                        <div className="paymentInput">
+                            <img src={currency_icon} alt="" />
+                            <select
+                                //dropbox for the currency
+                                className="paymentDropbox"
+                                //handling the change of the currency
+                                onChange={handleCurrencyChange}
+                                value={selectedCurrency}
+                            >
+                                <option value="ZAR">ZAR</option>
+                                <option value="USD">USD</option>
+                                <option value="GBP">GBP</option>
+                            </select>
+                        </div>
+
+                        {/* Payment Amount */}
+                        <div className="paymentLabel">
+                            <p>Payment Amount: </p>
+                        </div>
+                        <div className="paymentInput">
+                            <img src={money_icon} alt="" />
+                            <input
+                                type="text"
+                                value={paymentAmount}
+                                onChange={handlePaymentAmountChange}
+                                placeholder="Payment Amount:"
+                            />
+                        </div>
+
+                        {/* Recipient bank */}
+                        <div className="paymentLabel">
+                            <p>Recipient Bank: </p>
+                        </div>
+                        <div className="paymentInput">
+                            <img src={bank_icon} alt="" />
+                            <select
+                                className="paymentDropbox"
+                                //handling the change of the bank
+                                onChange={handleBankChange}
+                                value={selectedBank}
+                            >
+                                <option value="STANDARD_BANK">Standard Bank</option>
+                                <option value="ABSA">ABSA</option>
+                                <option value="FNB">FNB</option>
+                                <option value="CAPITEC">Capitec</option>
+                                <option value="NEDBANK">Nedbank</option>
+                            </select>
+                        </div>
+
+                        {/* SWIFT Code */}
+                        <div className="paymentLabel">
+                            <p>Provider (SWIFT code): </p>
+                        </div>
+                        <div className="paymentInput">
+                            <img src={bank_icon} alt="" />
+                            <input
+                                type="text"
+                                value={swiftCode}
+                                placeholder="SWIFT Code"
+                                readOnly
+                            />
+                        </div>
+
+                        {/* Recipient name */}
+                        <div className="paymentLabel">
+                            <p>Recipient Name: </p>
+                        </div>
+                        <div className="paymentInput">
+                            <img src={user_icon} alt="" />
+                            <input
+                                type="text"
+                                value={recipientName}
+                                onChange={handleRecipientNameChange}
+                                placeholder="  Recipient Name:"
+                            />
+                        </div>
+                        {nameError && <p className="error-message">{nameError}</p>}
+
+                        {/* Recipient account number */}
+                        <div className="paymentLabel">
+                            <p>Recipient's account no: </p>
+                        </div>
+                        <div className="paymentInput">
+                            <img src={card_icon} alt="" />
+                            <input
+                                type="text"
+                                value={accountNumber}
+                                onChange={handleAccountNumberChange}
+                                placeholder="Recipient's Account No"
+                            />
+                        </div>
+                        {accountError && <p className="error-message">{accountError}</p>}
+                    </div>
+                    {/* Buttons */}
+
+                    <div className="paymentSubmit-container">
+                        <div className="paymentSubmit" onClick={handlePayment}>
+                            Pay Now
+                        </div>
+                        {/* Button to cancel the payment */}
+                        <div className="paymentSubmit" onClick={handleCancel}>
+                            Cancel
+                        </div>
+                    </div>
+
+                    {/*Will display the current success message*/}
+                    {message && (
+                        <p
+                            className={
+                                message.type === "success" ? "success-message" : "error-message"
+                            }
+                        >
+                            {message.text}
+                        </p>
+                    )}
+
+                    {/*the message that will show if the payment is successful*/}
+                    {showModal && (
+                        <div className="modal">
+                            <div className="modal-content">
+                                <h2>Payment Successful!</h2>
+                                <p>Your payment has been processed successfully.</p>
+                            </div>
+                        </div>
+                    )}
+                </>
             )}
         </div>
     );
