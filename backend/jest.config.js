96f609df8a80dc268116d19f289a905fa82b3837
ethan.schoonbee <64493919+EthanSchoonbee@users.noreply.github.com>
2024-10-15 11:58:30 +0200

    Update jest.config.js

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,8 +1,23 @@
 module.exports = {
-    transform: {
-        '^.+\\.m?js$': 'babel-jest', // Handle .js and .mjs files
-    },
-    moduleFileExtensions: ['js', 'mjs', 'json', 'node'], // Recognize .mjs extensions
-    testEnvironment: 'node', // Use Node.js environment
-    testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)', '**/?(*.)+(spec|test).mjs'], // Include mjs
+  transform: {
+    '^.+\\.m?js$': 'babel-jest', // Handle .js and .mjs files
+  },
+  moduleFileExtensions: ['js', 'mjs', 'json', 'node'], // Recognize .mjs extensions
+  testEnvironment: 'node', // Use Node.js environment
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[tj]s?(x)',
+    '**/?(*.)+(spec|test).mjs', // Include mjs
+  ],
+  collectCoverage: true,
+  coverageDirectory: 'coverage', // Directory where Jest will output coverage files
+  coverageReporters: ['json', 'lcov', 'text', 'clover'], // Report formats
+  // Specify which files to collect coverage for (if needed)
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}', // Adjust according to your source file types and paths
+    '!**/node_modules/**',
+    '!**/vendor/**',
+    // Optionally exclude test files from coverage
+    '!**/*.test.{js,jsx,ts,tsx}',
+  ],
 };
